3758b6cf3d5b362e8c834d5c2d991267082467aa
BogdanSahno <156195789+BogdanSahno@users.noreply.github.com>
2024-02-25 12:21:09 +0100

    Update Task_1.py

--- Task_1.py
+++ Task_1.py
@@ -1,18 +1,13 @@
 def caching_fibonacci():        
     cache = {}                  
-
     def fibonacci(n):           
         if n in cache:          
             return cache[n]     
-        
         if n <= 1:              
             return n
-
         cache[n] = fibonacci(n - 1) + fibonacci(n - 2)
         return cache[n]
-
     return fibonacci
-
 
 fib = caching_fibonacci()
 print(fib(10))
